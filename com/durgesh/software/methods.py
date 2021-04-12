a31332c93a96aca0ce68147b49e85a483886b459
Durgesh Agnihotri <45296392+durgeshiv@users.noreply.github.com>
2021-04-13 00:32:47 +0530

    Add-on
    1. Continue-break
    2. Methods

---
+++ com/durgesh/software/methods.py
@@ -0,0 +1,10 @@
+# A method or function in python is exactly same as in Scala except some syntax changes
+# as mentioned below, rest all are same
+
+def add(x,y):
+    result = x + y
+    return result
+
+print(add(10,5))
+
+# Here input arguments can also have some pre-defined values etc.
--- com/durgesh/software/py_continue-break_statements.py
+++ com/durgesh/software/py_continue-break_statements.py
@@ -4,7 +4,7 @@ data = [1, 2, 5, 0, 10, 11]
 a = 1
 for elem in data:
     a = a * elem
-print('The value of a is {}'.format(a)) ## This is giving an output = 0 because one of the element
+print('The value of a is {}'.format(a))  ## This is giving an output = 0 because one of the element
 # is 0 and anything multiply by 0, is a 0
 
 # Lets use continue statement here by which we can specify a particular
@@ -13,6 +13,26 @@ print('The value of a is {}'.format(a)) ## This is giving an output = 0 because 
 b = 1
 for elem in data:
     if elem == 0:
-        continue # Here we have specified that if element =0 execution should continue for next one
+        continue  # Here we have specified that if element =0 execution should continue for next one
     b = b * elem
-print('The value of b is {}'.format(b)) ## This is giving an output = 0 because one of the element
+print('The value of b is {}'.format(b))  ## This is giving an output = 0 because one of the element
+
+
+#### Break Statement
+
+# Consider a use case where-in we are required to search for a particular value in a list having millions of elements
+# Now, our desired element is present at some 4th of 5th position.
+# Thus, we would want execution to be stopped then and there in order to get good performance - Hence, we'll use break
+#
+#considering same list as above
+search = 2
+for value in data:
+    if value == search:
+        print('Value is found')
+        break
+
+# Thus a break is used to completely stop execution whereas
+#    continue is used to stop execution of current iteration only
+
+
+
